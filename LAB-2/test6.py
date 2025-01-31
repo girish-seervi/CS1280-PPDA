5791ccc6ef38d836bb8db6a607f3824eed586602
Girish Kailash <girishseervi891@gmail.com>
2025-01-31 10:44:45 +0530

    Add files via upload

---
+++ LAB-2/test6.py
@@ -0,0 +1,7 @@
+var = 12 # integer
+var = 12.45 # float
+a = b = 2 # multiple assignment
+i_am = True # bool
+
+
+
---
+++ LAB-2/test7.py
@@ -0,0 +1,7 @@
+a = 10
+print(type(a))  # Output: <class 'int'>
+b = 10.0
+print(type(b))  # Output: <class 'float'>
+c = True
+print(type(c))
+
